42e3e097fad878d5732e65ab40683479b10de560
Mahmoud Ali <Mahmoudali9316@outlook.com>
2022-08-16 00:05:04 +0200

    Creating logger

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,10 +1,12 @@
 import Axios from "axios";
 import { toast } from "react-toastify";
+import logger from "./logService";
+
 Axios.interceptors.response.use(null, error => {
   const expectedError =
     error.response && error.response.status >= 400 && error.response.status < 500;
   if (!expectedError) {
-    console.log("Logging the error", error);
+    logger.log(error);
     toast.error("An unexpected error occurred.");
   }
   return Promise.reject(error);
