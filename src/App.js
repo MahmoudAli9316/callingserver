956f5078497e04be64bfbdacef6aa00323c6349d
MahmoudAli <Mahmoudali9316@gmail.com>
2022-08-15 18:56:28 +0200

    making http requests

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
 import "./App.css";
 import React, { Component } from "react";
-import Axios from "axios";
-const apiEndPoint = "https://jsonplaceholder.typicode.com/posts";
+import httpServices from "./services/httpServices";
+import config from "./config.json";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 class App extends Component {
   state = {
     posts: [],
   };
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await Axios.post(apiEndPoint, obj);
+    const { data: post } = await httpServices.post(config.apiEndPoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
   };
-  handleUpdate = post => {
-    console.log("Updated", post);
+  handleUpdate = async post => {
+    post.title = "UPDATED";
+    await httpServices.put(`${config.apiEndPoint}/${post.id}`, post);
+    const posts = [...this.state.posts];
+    const index = posts.indexOf(post);
+    posts[index] = { ...post };
+    this.setState({ posts });
+    //httpServices.patch(`${config.apiEndPoint}/${post.id}`, { title: post.title });
   };
-  handleDelete = post => {
-    console.log("Deleted", post);
+  handleDelete = async post => {
+    const originalPosts = this.state.posts;
+
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({ posts });
+    try {
+      await httpServices.delete(`s${config.apiEndPoint}/${post.id}`);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        alert("This post has already been deleted.");
+      }
+      this.setState({ posts: originalPosts });
+    }
   };
   async componentDidMount() {
-    const { data: posts } = await Axios.get(apiEndPoint);
+    const { data: posts } = await httpServices.get(config.apiEndPoint);
     this.setState({ posts });
   }
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <button onClick={this.handleAdd} className="btn btn-primary m-3">
           Add
         </button>
